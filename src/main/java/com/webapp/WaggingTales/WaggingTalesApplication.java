a242be6860215ac39e451c6f922cc912ca52dd69
Tezah24 <tezah2000@gmail.com>
2023-04-14 12:16:03 +0100

    db connection and registration feature

--- src/main/java/com/webapp/WaggingTales/WaggingTalesApplication.java
+++ src/main/java/com/webapp/WaggingTales/WaggingTalesApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class WaggingTalesApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WaggingTalesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WaggingTalesApplication.class, args);
+    }
 
 }
--- src/main/java/com/webapp/WaggingTales/repository/RegisterRepository.java
+++ src/main/java/com/webapp/WaggingTales/repository/RegisterRepository.java
@@ -1,66 +1,10 @@
-//package com.webapp.WaggingTales.repository;
-//
-//import com.webapp.WaggingTales.entity.RegisterEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//@Repository
-//public interface RegisterRepository extends JpaRepository<RegisterEntity, String>{
-////    List<RegisterEntity> findByFirstName(String first_name);
-////    List<RegisterEntity> findByLastName(String last_name);
-////    List<RegisterEntity> findByUserEmail(String user_email);
-//
-//}
-//
-//package com.webapp.WaggingTales.repository;
-//
-//        import com.webapp.WaggingTales.entity.RegisterEntity;
-//
-//        import org.springframework.data.jpa.repository.JpaRepository;
-//        import org.springframework.data.repository.CrudRepository;
-//        import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface RegisterRepository extends JpaRepository<RegisterEntity, String> {
-//
-//}
-//last code
-//
-//package com.webapp.WaggingTales.repository;
-//
-//        import com.fasterxml.jackson.databind.JsonNode;
-//        import com.webapp.WaggingTales.entity.RegisterEntity;
-//        import org.springframework.data.jpa.repository.Query;
-//        import org.springframework.data.repository.CrudRepository;
-//        import org.springframework.data.repository.query.Param;
-//        import org.springframework.stereotype.Repository;
-//
-//        import java.util.List;
-//
-//
-//
-//        @Repository
-//        public interface RegisterRepository extends CrudRepository<RegisterEntity, String> {
-//                @Query("SELECT r FROM RegisterEntity r WHERE r.firstName = :firstName")
-//                List<JsonNode> findByfirstName(@Param("firstName") String firstName);
-//
-//                @Query("SELECT r FROM RegisterEntity r WHERE r.lastName = :lastName")
-//                List<JsonNode> findBylastName(@Param("lastName") String lastName);
-//
-//                @Query("SELECT r FROM RegisterEntity r WHERE r.userEmail = :userEmail")
-//                List<JsonNode> findByuserEmail(@Param("userEmail") String userEmail);
-//        }
-//
-//
-
-
 package com.webapp.WaggingTales.repository;
 
-import com.webapp.WaggingTales.bean.RegisterBean;
+import com.webapp.WaggingTales.entity.RegisterEntity;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface RegisterRepository extends JpaRepository<RegisterBean, String> {
-    void deleteById(String useremail);
+public interface RegisterRepository extends JpaRepository<RegisterEntity, String> {
+    void deleteById(@NotNull String useremail);
 }
